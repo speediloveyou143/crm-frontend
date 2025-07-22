c639a039133a1c63439881c208235d23ec126759
bharathi-reddy22 <mbharathireddy43@gmail.com>
2025-07-22 17:42:24 +0530

    feature-contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/userPages/Home";
 import AdminDashboard from "./pages/adminPages/AdminDashboard";
-// import Matches from "./pages/adminPages/Matches";
 import AllUsers from "./pages/adminPages/AllUsers";
-// import Vlsouttendance from "./pages/adminPages/Vlsouttendance";
-// import AllLinks from "./pages/adminPages/AllLinks";
 import About from "./pages/userPages/About";
 import ContactUs from "./pages/ContactUs";
-import PrivacyPolicy from "./pages/userPages/PrivacyPolicy";
 import Pricing from "./pages/userPages/Pricing";
 
 export default function App() {
@@ -25,14 +21,9 @@ export default function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<ContactUs />} />
-          <Route path="privacy-policy" element={<PrivacyPolicy />} />
         </Route>
-        {/* Admin Routes */}
         <Route path="/dashboard" element={<AdminDashboard />}>
           <Route path="all-users" element={<AllUsers />} />
-           {/* <Route path="matches" element={<Matches />} />
-          <Route path="vlsouttendance" element={<Vlsouttendance />} />
-          <Route path="alllinks" element={<AllLinks />} /> */}
         </Route>
       </Routes>
     </Router>
